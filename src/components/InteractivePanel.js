7dd2f6ed150110a569adf619dd79d642aeb1781d
Michael Mendoza <askmichaelmendoza@gmail.com>
2020-04-10 03:49:45 -0400

    Updated interactable item with prompt

--- src/components/InteractivePanel.js
+++ src/components/InteractivePanel.js
@@ -1,9 +1,8 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { exploreMode} from '../contexts/AppContext';
+import React, { useState } from 'react';
 
-function InteractiveItem(props) {
+function InteractiveCard(props) {
     return (
-        <div className = "interactive-item card flex-container" style={props.data.style} >
+        <div className="interactive-card card flex-container" style={props.data.style2}>
             <img className="interactive-avatar"  src="https://cms-assets.tutsplus.com/uploads/users/107/posts/24710/image/35-flat-animals-fox.jpg"/>
             <div className="flex-item">
                 <header>
@@ -18,6 +17,20 @@ function InteractiveItem(props) {
                     <i className="fas fa-share-alt"></i>
                 </div>
             </div>
+        </div>   
+    )
+}
+
+function InteractiveItem(props) {
+    const [show, setShow] = useState(false);
+
+    return (
+        <div className = "interactive-item flex-container"  style={props.data.style}>
+            <button className="interactive-prompt" onMouseEnter = {() => setShow(true)} onMouseLeave = {() => setShow(false)}>
+                <div className="speech-bubble"></div>
+            </button>
+            
+            { show && <InteractiveCard data={props.data}></InteractiveCard> }
         </div>
       );  
 }
@@ -27,11 +40,11 @@ function InteractivePanel(props) {
     const interactiveData = {
         novice: [
             { text: "7% of students explicity remarked that they had no previous experience with NPR: “I haven't heard of the website before, so that makes it less reliable.”", 
-              style: { top: '5%', left: '5%'}},
+              style: { top: '5%'}, style2: {left: '5%'}},
             { text: "16% of students focused on the casual tone: “The language has a tendency to be informal or even conversational” and “the writing didn't sound very scientific.”", 
-              style: { top: '45%', left: '5%'}},
+              style: { top: '45%'}, style2: {left: '5%'}},
             { text: "Novices noticed that the article had hyperlinks to many sources: “it actually has links to the study and information cited.” They did not click on the links, however. Nor did they understand what about the evidence cited was convincing or reliable. ",
-              style: { top: '74%', left: '20%'}}
+              style: { top: '74%'}, style2: {left: '20%'}}
         ],
         expert: [
             { text: "31% of students recognized NPR as a well-known source: “NPR is a nationally recognized radio station” and “NPR is a trusted news site.”",
@@ -42,10 +55,10 @@ function InteractivePanel(props) {
             style: { top: '10%', left: '10%'}}
         ]
     }
-    
+
   return (
     <div className="interactive-panel"> 
-        <img className="explore-image card" src={props.img} />
+        <img className="interactive-explore-image card" src={props.img} />
         { interactiveData.novice.map((item) => { return <InteractiveItem data={item}></InteractiveItem> })}
     </div>
   );
