2c52114c855c72a4d59dd6531fd8be34df2c1192
Michael Mendoza <askmichaelmendoza@gmail.com>
2020-04-14 12:28:18 -0400

    Adjusted styles of ExploreFilter

--- src/components/ExploreFilter.js
+++ src/components/ExploreFilter.js
@@ -10,10 +10,14 @@ function ExploreFilter() {
   return (
     <div className="explore-filter ">
 
-      <label> Explore article. Hover on chat bubbles to see novice or expert comments on article. </label>
-
       <div className="flex-container flex-center">
-        <label className="explore-show left">
+        <label> Explore article. Hover on chat bubbles to see novice or expert comments on article. </label>
+       
+        <span className="flex-item"></span>
+
+        <label className="explore-filter-label"> Filters: </label>
+
+        <label className="explore-show">
           Novice 
           <button className={ novice ? "active" : ""} onClick={handleNovice}> 
               <i className="fa fa-eye"></i>
@@ -21,14 +25,34 @@ function ExploreFilter() {
           </button>
         </label>
 
-        <span className="flex-item"></span>
-
-        <label className="explore-show right">
+        <label className="explore-show">
           Expert
           <button className={ expert ? "active" : ""} onClick={handleExpert}> 
               <i className="fa fa-eye"></i>
               <i className="fa fa-eye-slash"></i>
           </button>
+        </label>
+
+        <span style={ {"display":"none"} }> 
+          <label> Show </label>
+          <select>
+            <option selected value="grapefruit">default</option>
+            <option value="lime">novice</option>
+            <option value="coconut">expert</option>
+            <option value="mango">all</option>
+          </select> 
+        </span>
+      </div>
+
+      <div className="flex-container flex-center" style={ {"display":"none"} }>
+        <label className="top left">
+          <label className="underline">Novice</label>
+        </label>
+
+        <span className="flex-item"></span>
+
+        <label className="top right">
+        <label className="underline">Expert</label>
         </label>
       </div>
     </div>
