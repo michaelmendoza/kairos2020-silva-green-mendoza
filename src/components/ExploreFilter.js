f4e24f279d6a6378aa4879faeb8e4f549517d1d2
Michael Mendoza <askmichaelmendoza@gmail.com>
2020-04-13 23:23:39 -0400

    Added explaination for explore pages.

--- src/components/ExploreFilter.js
+++ src/components/ExploreFilter.js
@@ -8,26 +8,29 @@ function ExploreFilter() {
   const handleExpert = () =>  { setExpert(!expert); };
   
   return (
-    <div className="explore-filter flex-container flex-center">
-      
-      <label className="explore-show left">
-        Novice 
-        <button className={ novice ? "active" : ""} onClick={handleNovice}> 
-            <i class="fa fa-eye"></i>
-            <i class="fa fa-eye-slash"></i>
-        </button>
-      </label>
+    <div className="explore-filter ">
 
-      <span className="flex-item"></span>
+      <label> Explore article. Hover on chat bubbles to see novice or expert comments on article. </label>
 
-      <label className="explore-show right">
-        Expert
-        <button className={ expert ? "active" : ""} onClick={handleExpert}> 
-            <i class="fa fa-eye"></i>
-            <i class="fa fa-eye-slash"></i>
-        </button>
-      </label>
+      <div className="flex-container flex-center">
+        <label className="explore-show left">
+          Novice 
+          <button className={ novice ? "active" : ""} onClick={handleNovice}> 
+              <i class="fa fa-eye"></i>
+              <i class="fa fa-eye-slash"></i>
+          </button>
+        </label>
 
+        <span className="flex-item"></span>
+
+        <label className="explore-show right">
+          Expert
+          <button className={ expert ? "active" : ""} onClick={handleExpert}> 
+              <i class="fa fa-eye"></i>
+              <i class="fa fa-eye-slash"></i>
+          </button>
+        </label>
+      </div>
     </div>
   );
 }
