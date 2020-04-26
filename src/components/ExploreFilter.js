c95dc44ec03ba105a3619dac01401b4799f5359e
Michael Mendoza <askmichaelmendoza@gmail.com>
2020-04-26 06:18:14 -0400

    Removed unused components and styles.

--- src/components/ExploreFilter.js
+++ src/components/ExploreFilter.js
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../contexts/AppContext';
-import Switch from './Switch';
 
 function ExploreFilter() {
   const { novice, expert, setNovice, setExpert } = useContext(AppContext);
@@ -10,51 +9,30 @@ function ExploreFilter() {
   return (
     <div className="explore-filter ">
 
-      <div className="flex-container flex-center">
+      <div className="flex-container">
         <label> Explore article. Hover on chat bubbles to see novice or expert comments on article. </label>
        
         <span className="flex-item"></span>
 
         <label className="explore-filter-label"> Filters: </label>
 
-        <label className="explore-show">
+        <label className="explore-show flex-container">
           Novice 
           <button className={ novice ? "active" : ""} onClick={handleNovice}> 
               <i className="fa fa-eye"></i>
               <i className="fa fa-eye-slash"></i>
           </button>
         </label>
-
-        <label className="explore-show">
+        
+        <label className="explore-show flex-container">
           Expert
           <button className={ expert ? "active" : ""} onClick={handleExpert}> 
               <i className="fa fa-eye"></i>
               <i className="fa fa-eye-slash"></i>
           </button>
         </label>
-
-        <span style={ {"display":"none"} }> 
-          <label> Show </label>
-          <select>
-            <option selected value="grapefruit">default</option>
-            <option value="lime">novice</option>
-            <option value="coconut">expert</option>
-            <option value="mango">all</option>
-          </select> 
-        </span>
       </div>
 
-      <div className="flex-container flex-center" style={ {"display":"none"} }>
-        <label className="top left">
-          <label className="underline">Novice</label>
-        </label>
-
-        <span className="flex-item"></span>
-
-        <label className="top right">
-        <label className="underline">Expert</label>
-        </label>
-      </div>
     </div>
   );
 }
