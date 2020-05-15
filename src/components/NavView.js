f5623fcf4c0c75b603cb45f595386efbc0d9aa4d
Michael Mendoza <askmichaelmendoza@gmail.com>
2020-05-15 12:54:36 -0400

    Added active nav item based on clicked nav item

--- src/components/NavView.js
+++ src/components/NavView.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from '@reach/router';
 import { AppContext } from '../contexts/AppContext';
 
@@ -13,19 +13,23 @@ function NavView() {
   const handleMobileNav = () => { setMobileNav(false) }; 
   const navClass = "nav-view " + (mobileNav ? 'nav-mobile-on':'');
 
+  const [ active, setActive ] = useState(-1);
+  const activeNav = (index) => {  return active == index ? "active" : "" }    
+  const setActiveNav = (index) => { setActive(index); handleMobileNav(); }
+  
   return (
     <nav className={navClass}> 
       <span className="nav-close"> <button onClick={handleMobileNav}> <i className="fas fa-times"></i> </button> </span> 
       <section className="menu-nav">
         <h2> Menu </h2>
-      <ul>
-          <li> <Link to="/"> <button onClick={handleMobileNav}> Introduction </button> </Link> </li>
-          <li> <Link to="methods" ><button onClick={handleMobileNav}> Methods </button> </Link> </li>
-          <li> <Link to="findings" ><button onClick={handleMobileNav}> Findings </button> </Link> </li>
-          <li> <Link to="context" ><button onClick={handleMobileNav}> Context and Discussion </button> </Link> </li>
-          <li> <Link to="implications" ><button onClick={handleMobileNav}> Implications </button> </Link> </li>
-          <li> <Link to="references"><button onClick={handleMobileNav}> References </button></Link></li>
-          <li> <Link to="contact" ><button onClick={handleMobileNav}> Contact </button> </Link> </li>
+      <ul> 
+          <li className={ activeNav(0) }> <Link to="/"> <button onClick={ () => setActiveNav(0) }> Introduction </button> </Link> </li>
+          <li className={ activeNav(1) }> <Link to="methods" ><button onClick={ () => setActiveNav(1) }> Methods </button> </Link> </li>
+          <li className={ activeNav(2) }> <Link to="findings" ><button onClick={ () => setActiveNav(2) }> Findings </button> </Link> </li>
+          <li className={ activeNav(3) }> <Link to="context" ><button onClick={ () => setActiveNav(3) }> Context and Discussion </button> </Link> </li>
+          <li className={ activeNav(4) }> <Link to="implications" ><button onClick={ () => setActiveNav(4) }> Implications </button> </Link> </li>
+          <li className={ activeNav(5) }> <Link to="references"><button onClick={ () => setActiveNav(5) }> References </button></Link></li>
+          <li className={ activeNav(6) }> <Link to="contact" ><button onClick={ () => setActiveNav(6) }> Contact </button> </Link> </li>
         </ul> 
       </section>
       <section className="explore-nav"> 
