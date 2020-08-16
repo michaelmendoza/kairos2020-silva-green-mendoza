93baa08398593c47f6c831faaaea312cfa856c6b
Michael Mendoza <askmichaelmendoza@gmail.com>
2020-08-16 11:07:37 -0600

    Updated server-public.js

--- server-public.js
+++ server-public.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
